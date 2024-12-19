b1c43f718cce9e1f4559b8e070ffd2babd1133c7
julia <iuliakudishina@yandex.ru>
2024-12-19 21:59:09 +0300

    тест2

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -1,10 +1,18 @@
 describe('Проверяем доступность приложения', function () {
+  // const testUrl = 'http://localhost:4000';
   it('Сервис должен быть доступен по адресу localhost:4000', function () {
-    cy.visit('http://localhost:4000');
+    cy.visit('/');
   });
 });
 
 describe('Тестирование конструктора бургера', () => {
+  const SELECT_BUNS_TEXT = 'Выберите булки';
+  const SELECT_FILLING_TEXT = 'Выберите начинку';
+  const BUNS_SECTION_TITLE = 'Булки';
+  const FILLINGS_SECTION_TITLE = 'Начинки';
+  const MODAL_SELECTOR = '#modals';
+  const BODY_SELECTOR = 'body';
+
   //  ингредиенты с сервера
   before(() => {
     cy.intercept('GET', 'api/ingredients', {
@@ -18,42 +26,46 @@ describe('Тестирование конструктора бургера', () 
       fixture: 'user.json'
     }).as(`${'user'}`);
     cy.setCookie('accessToken', 'mockAccessToken');
-    localStorage.setItem('refreshToken', 'mockReshToken');
+    localStorage.setItem('refreshToken', 'mockRefreshToken');
   });
 
   // Выполнить в конце
   afterEach(() => {
-    cy.setCookie('accessToken', '');
-    window.localStorage.setItem('refreshToken', '');
+    cy.clearCookies();
+    localStorage.removeItem('refreshToken');
   });
 
   beforeEach('Открытие главной страницы', () => {
-    cy.visit('http://localhost:4000');
+    cy.visit('/');
   });
 
   // сбор и оформление заказа
   describe('Тестирование сбор заказа и его оформление', () => {
     describe('Тестирование добавления ингредиентов в конструктор', () => {
       it('Добавление булок в конструктор', () => {
-        cy.get('div').contains('Выберите булки').should('exist');
-        cy.get('h3').contains('Булки').next('ul').contains('Добавить').click();
-        cy.get('div').contains('Выберите булки').should('not.exist');
-      });
-
-      it('Добавление начинок в конструктор', () => {
-        cy.get('div').contains('Выберите начинку').should('exist');
+        cy.get('div').contains(SELECT_BUNS_TEXT).should('exist');
         cy.get('h3')
-          .contains('Начинки')
+          .contains(BUNS_SECTION_TITLE)
           .next('ul')
           .contains('Добавить')
           .click();
-        cy.get('div').contains('Выберите начинку').should('not.exist');
+        cy.get('div').contains(SELECT_BUNS_TEXT).should('not.exist');
+      });
+
+      it('Добавление начинок в конструктор', () => {
+        cy.get('div').contains(SELECT_FILLING_TEXT).should('exist');
+        cy.get('h3')
+          .contains(FILLINGS_SECTION_TITLE)
+          .next('ul')
+          .contains('Добавить')
+          .click();
+        cy.get('div').contains(SELECT_FILLING_TEXT).should('not.exist');
       });
 
       it('Добавление соусов в конструктор', () => {
-        cy.get('div').contains('Выберите начинку').should('exist');
+        cy.get('div').contains(SELECT_FILLING_TEXT).should('exist');
         cy.get('h3').contains('Соусы').next('ul').contains('Добавить').click();
-        cy.get('div').contains('Выберите начинку').should('not.exist');
+        cy.get('div').contains(SELECT_FILLING_TEXT).should('not.exist');
       });
 
       it('Сбор бургера и оформление заказа', () => {
@@ -64,9 +76,13 @@ describe('Тестирование конструктора бургера', () 
         }).as(`${'order'}`);
 
         //Сбор бургера
-        cy.get('h3').contains('Булки').next('ul').contains('Добавить').click();
         cy.get('h3')
-          .contains('Начинки')
+          .contains(BUNS_SECTION_TITLE)
+          .next('ul')
+          .contains('Добавить')
+          .click();
+        cy.get('h3')
+          .contains(FILLINGS_SECTION_TITLE)
           .next('ul')
           .contains('Добавить')
           .click();
@@ -81,12 +97,12 @@ describe('Тестирование конструктора бургера', () 
         cy.contains('6655').should('exist');
 
         // закрытие esc
-        cy.get(`body`).type('{esc}');
-        cy.get(`body`).type('{esc}');
+        cy.get(BODY_SELECTOR).type('{esc}');
+        cy.get(BODY_SELECTOR).type('{esc}');
 
         // проверка очистки
-        cy.contains('Выберите начинку').should('exist');
-        cy.contains('Выберите булки').should('exist');
+        cy.contains(SELECT_FILLING_TEXT).should('exist');
+        cy.contains(SELECT_BUNS_TEXT).should('exist');
       });
     });
   });
@@ -97,13 +113,13 @@ describe('Тестирование конструктора бургера', () 
       cy.contains('Говяжий метеорит (отбивная)').click();
     });
     it('Закрытие модального окна по нажатию на крестик', () => {
-      cy.get('#modals').find('button').click();
-      cy.get('#modals').should('be.empty');
+      cy.get(MODAL_SELECTOR).find('button').click();
+      cy.get(MODAL_SELECTOR).should('be.empty');
       cy.get('div').contains('Детали ингредиента').should('not.exist');
     });
     it('Закрытие модального окна по нажатию оверлей', () => {
       cy.contains('Детали ингредиента').should('exist');
-      cy.get(`body`).type('{esc}');
+      cy.get(BODY_SELECTOR).type('{esc}');
       cy.contains('Детали ингредиента').should('not.exist');
     });
   });
